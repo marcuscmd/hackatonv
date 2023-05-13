de12d3e375e5217c3aba0fb7643436f7139d01e2
Chriffy-Herculano <chriffy123@gmail.com>
2023-05-13 16:21:30 -0300

    Telas novas
    Telas novas

---
+++ hackatonV/lib/propriedade/propriedade_page.dart
@@ -0,0 +1,237 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+
+class PropriedadePage extends StatelessWidget {
+  const PropriedadePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        child: ListView(children: [
+          Container(
+            height: 320,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/backgroud.png'),
+                    fit: BoxFit.fill)),
+            child: Stack(children: [
+              Center(
+                child: Container(
+                  margin: const EdgeInsets.only(bottom: 150),
+                  width: 210,
+                  height: 210,
+                  //left: 75,
+                  child: const Icon(
+                    Icons.assignment_turned_in_outlined,
+                    size: 115,
+                  ),
+                ),
+              ),
+            ]),
+          ),
+          Container(
+            child: Stack(children: [
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.only(top: 0),
+                      margin: const EdgeInsets.only(top: 0, right: 20),
+                      child: const Icon(
+                        Icons.assignment_turned_in_outlined,
+                        size: 50,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ]),
+          ),
+          Container(
+            child: Stack(children: [
+              Center(
+                child: Container(
+                  margin: const EdgeInsets.only(top: 5),
+                  child: const Center(
+                    child: Text(
+                      'Informações da Propriedade',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Color.fromARGB(230, 12, 0, 0),
+                        fontSize: 37,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ]),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(30),
+            child: Column(
+              children: [
+                const SizedBox(height: 15),
+                Container(
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Color.fromRGBO(19, 149, 167, 0.3),
+                            blurRadius: 20.0,
+                            offset: Offset(0, 10))
+                      ]),
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(top: 10, bottom: 10),
+                        padding: const EdgeInsets.all(8),
+                        decoration: const BoxDecoration(
+                            border: Border(
+                                bottom: BorderSide(
+                          color: Color.fromRGBO(200, 200, 200, 1),
+                        ))),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Nome da Propiedade',
+                              hintStyle: TextStyle(color: Colors.grey[400])),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 15),
+                Container(
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Color.fromRGBO(19, 149, 167, 0.3),
+                            blurRadius: 20.0,
+                            offset: Offset(0, 10))
+                      ]),
+                  child: Column(children: [
+                    Container(
+                      margin: const EdgeInsets.only(top: 10, bottom: 10),
+                      padding: const EdgeInsets.all(8),
+                      decoration: const BoxDecoration(
+                          border: Border(
+                              bottom: BorderSide(
+                        color: Color.fromRGBO(200, 200, 200, 1),
+                      ))),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Tamanho do Hectar',
+                            hintStyle: TextStyle(color: Colors.grey[400])),
+                      ),
+                    ),
+                  ]),
+                ),
+                const SizedBox(height: 15),
+                Container(
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Color.fromRGBO(19, 149, 167, 0.3),
+                            blurRadius: 20.0,
+                            offset: Offset(0, 10))
+                      ]),
+                  child: Column(children: [
+                    Container(
+                      margin: const EdgeInsets.only(top: 10, bottom: 10),
+                      padding: const EdgeInsets.all(8),
+                      decoration: const BoxDecoration(
+                          border: Border(
+                              bottom: BorderSide(
+                        color: Color.fromRGBO(200, 200, 200, 1),
+                      ))),
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly,
+                          LengthLimitingTextInputFormatter(8),
+                          MaskTextInputFormatter(
+                              mask: '##/##/####',
+                              filter: {"#": RegExp(r'[0-9]')})
+                        ],
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Data do Plantio',
+                            hintStyle: TextStyle(color: Colors.grey[400])),
+                      ),
+                    ),
+                  ]),
+                ),
+                const SizedBox(height: 15),
+                Container(
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Color.fromRGBO(19, 149, 167, 0.3),
+                            blurRadius: 20.0,
+                            offset: Offset(0, 10))
+                      ]),
+                  child: Column(children: [
+                    Container(
+                      margin: const EdgeInsets.only(top: 10, bottom: 10),
+                      padding: const EdgeInsets.all(8),
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            color: Color.fromRGBO(200, 200, 200, 1),
+                          ),
+                        ),
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Tipo do Plantio',
+                            hintStyle: TextStyle(color: Colors.grey[400])),
+                      ),
+                    ),
+                  ]),
+                ),
+                const SizedBox(height: 15),
+                const SizedBox(height: 30),
+                Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      gradient: const LinearGradient(colors: [
+                        Color.fromRGBO(114, 219, 233, 1),
+                        Color.fromRGBO(37, 130, 173, 0.945)
+                      ])),
+                  child: const Center(
+                    child: Text('Salvar',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 37,
+                        )),
+                  ),
+                ),
+                const SizedBox(height: 30),
+              ],
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
